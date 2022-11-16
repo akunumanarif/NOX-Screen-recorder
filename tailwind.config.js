1f335d43bdf674d88d9be2c76e22471c3a7eb896
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-17 02:02:18 +0700

    install tailwindcss config and modify

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["*.html"],
+  mode: "jit",
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Fira sans", "sans-serif"],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
